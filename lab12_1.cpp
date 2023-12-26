6472ee26dadc8c63ac7d982a9c12f8977583120b
Your Name <you@example.com>
2023-12-26 14:20:07 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
+#include <ctime>
 
-int fibonacci(int x);
+long long  fibonacci(int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-
-
-
-    return 0; 
+   
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int x){
+
+long long fibonacci(int x){
     if (x>1){
         return fibonacci(x-1) + fibonacci(x-2);
     }else{
